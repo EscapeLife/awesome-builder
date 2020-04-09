f9e6665160c60f847450bad22a5e18f929aefdaf
Escape <240901017@qq.com>
2020-04-10 00:06:20 +0800

    feat: add docker entrypoint script to cloudreve tool

---
+++ dockerfiles/cloudreve/docker/docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+WORK_DIR='/opt/cloudreve'
+cd ${WORK_DIR}
+
+mkdir -pv /data/logs
+if [ ! -n "${DOCKER_DEBUG}" ]; then
+    exec 1>>/data/logs/init.log 2>&1
+fi
+
+rm -rf /etc/nginx/sites-enabled/* /etc/supervisor/conf.d/*
+cp -rf /docker/nginx/cloudreve.conf /etc/nginx/conf.d/
+cp -rf /docker/supervisor/supervisord.conf /etc/supervisor/supervisord.conf
+if [[ ${type_mold} == 'standard' ]]; then
+    cp -rf /docker/supervisor/conf.d/standard-cloudreve.conf /etc/supervisor/conf.d/
+else
+    cp -rf /docker/supervisor/conf.d/minimize-cloudreve.conf /etc/supervisor/conf.d/
+fi
+
+supervisord -n -c /etc/supervisor/supervisord.conf
