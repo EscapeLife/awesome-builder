9fbdf50d251faf3cb80e3d4cdd79b6b19b4bf8a0
Escape <240901017@qq.com>
2020-04-10 10:46:43 +0800

    improvement: add cloudreve.db link to data dir

--- dockerfiles/cloudreve/docker/docker-entrypoint.sh
+++ dockerfiles/cloudreve/docker/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 WORK_DIR='/opt/cloudreve'
 cd ${WORK_DIR}
 
-mkdir -pv /data/logs /data/uploads
+mkdir -pv /data/logs /data/uploads /data/db
 if [ ! -n "${DOCKER_DEBUG}" ]; then
     exec 1>>/data/logs/init.log 2>&1
 fi
@@ -14,6 +14,7 @@ rm -rf /etc/nginx/sites-enabled/* /etc/supervisor/conf.d/*
 cp -rf /docker/nginx/cloudreve.conf /etc/nginx/conf.d/
 cp -rf /docker/supervisor/supervisord.conf /etc/supervisor/supervisord.conf
 ln -sf /data/uploads /opt/cloudreve/uploads
+ln -sf /data/db /opt/cloudreve/cloudreve.db
 if [[ ${type_mold} == 'standard' ]]; then
     cp -rf /docker/supervisor/conf.d/standard-cloudreve.conf /etc/supervisor/conf.d/
 else
