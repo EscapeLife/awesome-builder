cffbb14796f569238d357a9d1fe45bf1c04f3b07
Escape <240901017@qq.com>
2020-04-10 15:39:17 +0800

    feat: the HTTP and HTTPS protocols are supported when the container is started

--- dockerfiles/cloudreve/docker/docker-entrypoint.sh
+++ dockerfiles/cloudreve/docker/docker-entrypoint.sh
@@ -10,15 +10,23 @@ if [ ! -n "${DOCKER_DEBUG}" ]; then
     exec 1>>/data/logs/init.log 2>&1
 fi
 
+# nginx
 rm -rf /etc/nginx/sites-enabled/* /etc/supervisor/conf.d/*
-cp -rf /docker/nginx/cloudreve.conf /etc/nginx/conf.d/
+if [[ ${SCHEMA_MOLD} == 'https' ]]; then
+    cp -rf /docker/nginx/https.conf /etc/nginx/conf.d/
+else
+    cp -rf /docker/nginx/http.conf /etc/nginx/conf.d/
+fi
+
+# supervisor
 cp -rf /docker/supervisor/supervisord.conf /etc/supervisor/supervisord.conf
 ln -sf /data/uploads /opt/cloudreve/uploads
 ln -sf /data/db /opt/cloudreve/cloudreve.db
-if [[ ${type_mold} == 'standard' ]]; then
+if [[ ${TYPE_MOLD} == 'standard' ]]; then
     cp -rf /docker/supervisor/conf.d/standard-cloudreve.conf /etc/supervisor/conf.d/
 else
     cp -rf /docker/supervisor/conf.d/minimize-cloudreve.conf /etc/supervisor/conf.d/
 fi
 
+# main
 supervisord -n -c /etc/supervisor/supervisord.conf
