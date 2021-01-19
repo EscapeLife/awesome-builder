936ce73469b983afa5384754891bc5d947be30e9
Escape <240901017@qq.com>
2021-01-19 14:56:10 +0800

    improvement: support max_wal_szie to user by env

--- dockerfiles/postgresql/patchdb.d/002_max_wal_size.sh
+++ dockerfiles/postgresql/patchdb.d/002_max_wal_size.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sed -i "s/^\#max_wal_size = .*/max_wal_size = 16MB/g" "${PGDATA}"/postgresql.conf
+sed -i "s/^\#max_wal_size = .*/max_wal_size = ${POSTGRES_MAX_WAL_SIZE:-16}MB/g" "${PGDATA}"/postgresql.conf
 sed -i "s/^\#min_wal_size = .*/min_wal_size = 2MB/g" "${PGDATA}"/postgresql.conf
