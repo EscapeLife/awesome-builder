d8b4355a920eec20eafd5ad1f8051a94cc1cbe95
Escape <240901017@qq.com>
2021-01-19 14:56:37 +0800

    improvement: support max_connections to user by env

--- dockerfiles/postgresql/patchdb.d/000_max_connections.sh
+++ dockerfiles/postgresql/patchdb.d/000_max_connections.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sed -i 's/max_connections = [0-9]\+/max_connections = 1000/g' "${PGDATA}"/postgresql.conf
+sed -i "s/max_connections = [0-9]\+/max_connections = ${POSTGRES_MAX_CONNECTIONS:-1000}/g" "${PGDATA}"/postgresql.conf
