409a0ae0034a0c430b4e08970648bfb39100aa3e
Escape <240901017@qq.com>
2021-01-25 11:13:39 +0800

    improvement: update single setup.sh add aliyun mirrors

--- common/vagrant/single/setup.sh
+++ common/vagrant/single/setup.sh
@@ -1,1 +1,22 @@
+#!/usr/bin/env bash
 
+set -ex
+
+# ubuntu aliyun mirrors
+sudo cp /etc/apt/sources.list{,.bak}
+sudo cat >/etc/apt/sources.list <<EOF
+deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+EOF
+
+# apt update
+sudo apt update
+sudo apt dist-upgrade
