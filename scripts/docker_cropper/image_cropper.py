5180a9819b37b2e23308547503888fa870f543f0
Escape <240901017@qq.com>
2021-03-26 10:26:27 +0800

    improvement: add code comments and change variable name rules

--- scripts/docker_cropper/image_cropper.py
+++ scripts/docker_cropper/image_cropper.py
@@ -8,24 +8,27 @@ import sh
 
 
 class Cropper:
-    """A mirrored patch pack generation tool"""
+    """镜像补丁包生成工具"""
 
     def __init__(self, base_image, latest_image):
-        self._base_image = base_image
-        self._latest_image = latest_image
+        self.__base_image = base_image
+        self.__latest_image = latest_image
 
-    def __run(self, cmd):
+    def _run(self, cmd):
+        """执行SHELL命令"""
         return sh.bash('-c', str(' '.join(cmd))).stdout
 
-    def __get_layers_from_image(self, path):
-        output = self.__run(['tar', '-xf', path, 'manifest.json', '-O'])
+    def _get_layers_from_image(self, path):
+        """获取manifest文件中的镜像层信息"""
+        output = self._run(['tar', '-xf', path, 'manifest.json', '-O'])
         layers = json.loads(output)[0]['Layers']
         return [layer.split('/')[0] for layer in layers]
 
-    def __get_intersection_layers(self):
+    def _get_intersection_layers(self):
+        """获取公共的镜像层对应Hash列表"""
         click.echo(click.style('=== 查看Docker镜像版本的差异 ===', fg='red'))
-        base_image_manifest = set(self.__get_layers_from_image(self._base_image))
-        latest_image_manifest = set(self.__get_layers_from_image(self._latest_image))
+        base_image_manifest = set(self._get_layers_from_image(self._base_image))
+        latest_image_manifest = set(self._get_layers_from_image(self._latest_image))
         image_intersection_list = list(base_image_manifest.intersection(latest_image_manifest))
 
         click.echo(click.style('>>> The base image manifest ...', fg='green'))
@@ -37,28 +40,28 @@ class Cropper:
         return image_intersection_list
 
     def export_lite_tar(self):
-        dist_dir = self._latest_image.rstrip('.tar') + '_lite'
-        dist_tar = self._latest_image.rstrip('.tar') + '_lite.tar'
+        """打包lite镜像补丁包"""
+        dist_dir = self.__latest_image.rstrip('.tar') + '_lite'
+        dist_tar = self.__latest_image.rstrip('.tar') + '_lite.tar'
         if os.path.isdir(dist_dir):
             shutil.rmtree(dist_dir)
         os.makedirs(dist_dir)
-        exclude_layers = ['--exclude=' + layer for layer in self.__get_intersection_layers()]
+        exclude_layers = ['--exclude=' + layer for layer in self._get_intersection_layers()]
 
         click.echo(click.style('=== 生成Docker版本差异补丁包 ===', fg='red'))
-        click.echo(click.style('>>> To ceate docker image patch tar ...', fg='green'))
-        self.__run(['tar', '-xpf', self._latest_image, '-C', dist_dir] + exclude_layers)
-        self.__run(['tar', '-C', dist_dir, '-cpf', dist_tar, '.'])
+        click.echo(click.style('>>> To create docker image patch tar ...', fg='green'))
+        self._run(['tar', '-xpf', self.__latest_image, '-C', dist_dir] + exclude_layers)
+        self._run(['tar', '-C', dist_dir, '-cpf', dist_tar, '.'])
         shutil.rmtree(dist_dir)
         click.echo(click.style(f'>>> The docker image patch is {dist_tar} ...', fg='green'))
-        pprint(str(self.__run(['tar', 'tf', dist_tar])).split('\\n'), indent=4)
+        pprint(str(self._run(['tar', 'tf', dist_tar])).split('\\n'), indent=4)
 
 
 @click.command()
-@click.option('-b', '--base-image', type=click.Path(exists=True), help='The previous version was a mirror image')
+@click.option('-b', '--base-image', type=click.Path(exists=True), help='The previous version of the mirror')
 @click.option('-l', '--latest-image', type=click.Path(exists=True), help='The latest version of the mirror')
 @click.pass_context
 def create_patch(ctx, base_image, latest_image):
-    """A mirrored patch pack generation tool"""
     ctx.obj = Cropper(base_image, latest_image)
     ctx.obj.export_lite_tar()
 
