9d4d50db063c35a3c8ef094d2236eec6992dac8e
Escape <240901017@qq.com>
2021-04-08 16:00:33 +0800

    improvement: update cve_report with send msg again in same info

--- scripts/cve_report/tencent_security_report.py
+++ scripts/cve_report/tencent_security_report.py
@@ -15,8 +15,8 @@ USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 
 
 MATTERMOST_URL = ''
 PAYLOAD = {
-    "username": "",
-    "channel": "",
+    "username": "CI",
+    "channel": "escape",
     "icon_url": ""
 }
 
@@ -46,14 +46,14 @@ def _execute_sqlite_db(sql):
         conn.close()
 
 
-def _send_msg_mm(title, link):
+def send_msg_mm(channel, title, link):
+    PAYLOAD.update({"channel": f"{channel}"})
     PAYLOAD.update({"text": f":robot: **[腾讯安全漏洞提示]** - [{title}]({link})"})
     r = requests.post(MATTERMOST_URL, data=json.dumps(PAYLOAD))
     if r.status_code == 200:
         logger.debug('Send vulnerability info to mattermost successfully ...')
     else:
         logger.error('Send vulnerability info to mattermost failed ...')
-        sys.exit()
 
 
 def init_sqlite_db():
@@ -61,7 +61,7 @@ def init_sqlite_db():
         "CREATE TABLE vulnerability( \
             id     INTEGER    PRIMARY KEY  AUTOINCREMENT, \
             title  CHAR(100)  NOT NULL, \
-            link   CHAR(150)  NOT NULL UNIQUE);")
+            link   CHAR(150)  NOT NULL);")
     logger.debug('Database table structure initialized successfully ...')
 
 
@@ -90,24 +90,24 @@ def insert_security_info(infos_list):
 
 def check_news_to_remind(infos_list):
     logger.debug('Begin updating server vulnerability information ...')
-    link_new_tuple = _execute_sqlite_db("SELECT link FROM vulnerability limit 8;")
-    link_new_list = _convert_tuple(link_new_tuple)
+    link_new_tuple = _execute_sqlite_db("SELECT link FROM vulnerability limit 16;")
+    link_new_list = list(_convert_tuple(link_new_tuple))
     for title, link in infos_list:
         if link not in link_new_list:
             logger.info(f"The [{title}] is lastest new, then insert to db and send msg ...")
             _execute_sqlite_db("INSERT INTO vulnerability (title, link) VALUES ('%s', '%s')" % (title, link))
-            _send_msg_mm(title, link)
+            send_msg_mm("@escape", title, link)
     logger.debug('Server vulnerability information update completed ...')
 
 
-def display_tencent_security_info():
-    logger.debug('Displays the latest version of the server vulnerability information ...')
-    pt = PrettyTable()
-    pt.field_names = ["id", "title", "link"]
-    tencent_security_info = _execute_sqlite_db("SELECT id, title, link FROM vulnerability limit 8;")
-    for sql_line in tencent_security_info:
-        pt.add_row(sql_line)
-    print(pt.get_string())
+# def display_tencent_security_info():
+#     logger.debug('Displays the latest version of the server vulnerability information ...')
+#     pt = PrettyTable()
+#     pt.field_names = ["id", "title", "link"]
+#     tencent_security_info = _execute_sqlite_db("SELECT id, title, link FROM vulnerability limit 8;")
+#     for sql_line in tencent_security_info:
+#         pt.add_row(sql_line)
+#     print(pt.get_string())
 
 
 def main():
@@ -137,11 +137,13 @@ if __name__ == '__main__':
     insert_security_info(infos_list)
 
     scheduler = BlockingScheduler(timezone="Asia/Shanghai")
-    scheduler.add_job(main, "cron", hour=22, minute=28, second=00)
+    # scheduler.add_job(main, 'interval', seconds=60)
+    scheduler.add_job(main, "cron", hour=8, minute=30, second=00)
 
     try:
-        logger.debug('Press Ctrl+C to exit ...')
+        logger.warning('Press Ctrl+C to exit ...')
         scheduler.start()
     except (KeyboardInterrupt, SystemExit):
-        logger.debug('Bye bye ...')
+        logger.warning('Bye bye ...')
+        send_msg_mm("@escape", "异常退出请处理", "")
         scheduler.shutdown()
